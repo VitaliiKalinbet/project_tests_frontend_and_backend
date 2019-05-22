5569667ca166df32111dd3ac197749d310ddb70d
Viktor Bulvarenko <silken.ua@gmail.com>
2019-05-22 16:46:24 +0300

    change some value in backend server.js, change name port to PORT

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,4 @@
-module.exports.port =  process.env.port || 3001;
-module.exports.db_url = 'mongodb://test:qwe123@ds157522.mlab.com:57522/goittests';
-module.exports.jwtsecret = 'secret';
+module.exports.port = process.env.PORT || 3001;
+module.exports.db_url =
+  "mongodb://test:qwe123@ds157522.mlab.com:57522/goittests";
+module.exports.jwtsecret = "secret";
